e421286c412b571daf423278420b03757ea34495
Naumenko Dmitrij <nopresent@gmail.com>
2021-10-24 18:34:43 +0300

    Добавил полей в класс продукт

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -7,6 +7,22 @@ public class Product {
     private Prod_category category; // Категория
     private Float price; // Цена
     private final String currency = "UAH"; // Валюта
+
+    public Product(int id, int count, String name, Prod_category category, Float price) {
+        this.id = id;
+        this.count = count;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
 
 enum Prod_category {
