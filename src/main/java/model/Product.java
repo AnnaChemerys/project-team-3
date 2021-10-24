775438e97c5b9a9735ed708d4aabf2541eed00ba
Naumenko Dmitrij <nopresent@gmail.com>
2021-10-24 18:09:01 +0300

    Добавил полей в класс продукт

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,5 +1,18 @@
 package model;
 
-
 public class Product {
+    private int id; // Идентификатор продукта
+    private int count; // Кол-во продуктов на складе
+    private String name; // Наименование
+    private Prod_category category; // Категория
+    private Float price; // Цена
+    private final String currency = "UAH"; // Валюта
 }
+
+enum Prod_category {
+     WATCHES,
+     SMARTPHONES,
+     COMPUTERS
+}
+
+
