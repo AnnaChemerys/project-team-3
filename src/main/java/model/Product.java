512d9ad92d44803d3944631c74ec0d30b2d75745
Naumenko Dmitrij <nopresent@gmail.com>
2021-10-25 21:50:25 +0300

    Добавил полей в класс Product.java

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,34 +1,72 @@
 package model;
 
-public class Product {
+abstract class Product {
     private int id; // Идентификатор продукта
-    private int count; // Кол-во продуктов на складе
+    private int count = 0; // Кол-во продуктов на складе
     private String name; // Наименование
-    private Prod_category category; // Категория
-    private Float price; // Цена
+    private Float amount; // Цена
     private final String currency = "UAH"; // Валюта
 
-    public Product(int id, int count, String name, Prod_category category, Float price) {
+    public void setId(int id) {
         this.id = id;
-        this.count = count;
+    }
+
+    public void setCount(int count) {
+        this.count += count;
+    }
+
+    public void setName(String name) {
         this.name = name;
-        this.category = category;
-        this.price = price;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public int getCount() {
         return count;
     }
 
-    public void setCount(int count) {
-        this.count = count;
+    public String getName() {
+        return name;
+    }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
     }
 }
 
-enum Prod_category {
-     WATCHES,
-     SMARTPHONES,
-     COMPUTERS
+class Computers extends Product {
+    public Computers(int id, int count, String name, Float amount) {
+        setId(id);
+        setCount(count);
+        setName(name);
+        setAmount(amount);
+    }
 }
 
+class Watches extends Product {
+    public Watches(int id, int count, String name, Float amount) {
+        setId(id);
+        setCount(count);
+        setName(name);
+        setAmount(amount);
+    }
+}
 
+class Phones extends Product {
+    public Phones(int id, int count, String name, Float amount) {
+        setId(id);
+        setCount(count);
+        setName(name);
+        setAmount(amount);
+    }
+}
